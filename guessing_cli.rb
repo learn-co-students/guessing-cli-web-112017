04aa9892398b39ceb1f1e677ad4dd1ab93abea98
nspp99b <chromepanthers@gmail.com>
2017-11-14 20:54:16 -0500

    solved using while, game toggle, rand range and control flow

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,20 @@
-# Code your solution here!
+def run_guessing_game
+  game_toggle = 1
+
+  while game_toggle == 1 do
+    puts "Guess a number between 1 and 6."
+    guess = gets.chomp
+    answer = rand(1..6).to_s
+
+    if guess == "exit"
+      puts "Goodbye!"
+      game_toggle = 0
+    else
+      if guess == answer
+        puts "You guessed the correct number!"
+      else
+        puts "The computer guessed #{answer}."
+      end
+    end
+  end
+end
